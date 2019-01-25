ce83d6177ac88945b397207af913406c95fc9616
Pjotr Savitski <gnum@tlu.ee>
2019-01-25 12:21:13 +0200

    Added one more case, this one has options to be provided.

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const materialInteractions = require('./lib/queries').materialInteractions;
 const materialInteractionsByVerb = require('./lib/queries').materialInteractionsByVerb;
 const materialScores = require('./lib/queries').materialScores;
 const contentTypeVerbs = require('./lib/queries').contentTypeVerbs;
+const materialScoresAndStatsForPeriodAndUrls = require('./lib/queries').materialScoresAndStatsForPeriodAndUrls;
 const writeDocumentsToCsv = require('./lib/csv').writeDocumentsToCsv;
 const chalk = require('chalk');
 const os = require('os');
+const durationParse = require('iso8601-duration').parse;
+const durationToSeconds = require('iso8601-duration').toSeconds;
 
-function buildSingleCase(db, singleCase) {
-  const promise = singleCase.function(db)
+function buildSingleCase(db, singleCase, options) {
+  const promise = singleCase.function(db, options)
   .then(documents => {
     writeDocumentsToCsv(documents, singleCase.file, singleCase.csvCallback, singleCase.csvHeader);
   });
@@ -55,13 +58,52 @@ cases.contentTypeVerbs = {
   },
   csvHeader: ['content type', 'verbs']
 };
+cases.materialScoresAndStatsForPeriodAndUrls = {
+  title: 'Material scores and other statistics limited to certain period and object URLs',
+  file: 'material-scores-and-stats-for-period-and-urls.csv',
+  function: materialScoresAndStatsForPeriodAndUrls,
+  optionsFunction: ()=> {
+    const options = [];
+    // TODO Might need to make sure that the date is forced into GMT/UTC
+    options.from = new Date(process.argv[3]);
+    options.to = new Date(process.argv[4]);
+    options.ids = process.argv.slice(5);
+
+    if (isNaN(options.from) || isNaN(options.to)) {
+      throw new Error('One of the dates is not suitable!');
+    }
+
+    if (options.to < options.from) {
+      throw new Error('Beginning date is after the end date!');
+    }
+
+    if (options.ids.length < 1) {
+      throw new Error('At least one URL has to be provided!');
+    }
+
+    return options;
+  },
+  csvCallback: (document, stringifier) => {
+    let averageDuration;
+    if (document.durations.length) {
+      const durations = document.durations.map((duration) => {
+        return durationToSeconds(durationParse(duration));
+      });
+      averageDuration = durations.reduce((total, duration) => {
+        return total + duration;
+      }) / durations.length;
+    }
+    stringifier.write([document._id.url, document._id.verb, document.name, document.sessions.length, averageDuration, document.successCount, document.min, document.max, document.rawMin, document.rawMax, document.rawAvg]);
+  },
+  csvHeader: ['URL', 'verb', 'name', 'unique sessions count', 'average duration (seconds)', 'success count', 'minimum possible score', 'maximum possible score', 'student minimum score (raw)', 'student maximum score (raw)', 'student average score (raw)']
+};
 cases.all = {
-  title: 'All',
+  title: 'All (except for ones that require additional options)',
   function: (client, db) => {
     const promises = [];
 
     Object.keys(cases).forEach(single => {
-      if (single !== 'all') {
+      if (single !== 'all' && !('optionsFunction' in cases[single])) {
         const promise = buildSingleCase(db, cases[single]);
 
         promises.push(promise);
@@ -99,7 +141,19 @@ connect()
   if (queryCase === 'all') {
     cases[queryCase].function(client, db);
   } else {
-    buildSingleCase(db, cases[queryCase])
+    let options = [];
+    if (cases[queryCase].optionsFunction) {
+      try {
+        options = cases[queryCase].optionsFunction();
+      } catch(e) {
+        console.error(chalk.red(e.message));
+        client.close();
+        return;
+      }
+      console.log(chalk.green('Options:'));
+      console.log(options);
+    }
+    buildSingleCase(db, cases[queryCase], options)
     .then(() => {
       client.close();
     });
